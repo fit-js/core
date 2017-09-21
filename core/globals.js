64e63a89344c7032fbfa07cfd69eaf405ac65de6
Tomas Lakauskas <thfrlw@icloud.com>
2017-09-21 16:44:02 +0300

    eslint update

--- core/globals.js
+++ core/globals.js
@@ -2,10 +2,10 @@ let globals = {};
 
 export function is (name) {
 	return globals.hasOwnProperty(name);
-};
+}
 export function get (name) {
 	return globals[name];
-};
+}
 export function set (name, content) {
 	globals[name] = content;
-};
+}
