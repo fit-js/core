301d1f9b565976deaf594f2973ab5199e521d058
Tomas Lakauskas <thfrlw@icloud.com>
2017-10-03 16:11:07 +0300

    pass core plugin init

--- core/loader.js
+++ core/loader.js
@@ -1,19 +1,19 @@
 import * as path from 'path';
 import * as process from 'process';
-import { args, utils, config } from './';
+import * as core from './';
 
 function local (module_name, opts) {
 	let relative = path.join (process.cwd(), 'node_modules', module_name);
 	let local = path.join (process.cwd(), '..', module_name);
 
 	return opts != true ?
-		relative : args.emulateLocal() ?
-			utils.moduleResolve (local) ?
+		relative : core.args.emulateLocal() ?
+			core.utils.moduleResolve (local) ?
 				local : relative : relative;
 }
 
 export default function () {
-	let use = config.use();
+	let use = core.config.use();
 
 	if ( use.length ) {
 		use.forEach((item) => {
@@ -21,18 +21,18 @@ export default function () {
 			console.log ((' '+ path.relative (process.cwd(), load) +' ').gray.bold.inverse);
 
 			try {
-				require (load).init (item.config);
+				require (load).init (item.config, core);
 				console.log (' Loaded '.green.bold.inverse);
 			}
 			catch (e) {
-				console.log (' Failed '.red.bold.inverse +':', e.code ? e.code.red : e);
+				console.log (' Failed '.red.bold.inverse +':', e.toString().grey);
 			}
 
 		});
 
 		console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~'.rainbow);
 	} else {
-		utils.error ('No Bundles found.');
+		core.utils.error ('No Bundles found.');
 	}
 
 	return Promise.resolve();
